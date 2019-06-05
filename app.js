f87705f4230a4ccdf60e2271acc1d3a850f3478f
lprice93 <lprice@wilbron.com>
2019-06-05 16:09:48 -0500

    final lab

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-let friends = ["Halle", "Jasmine", "Kimani", "Haily", "Jalon"];
+let btn = document.getElementsByClassName("sing");
+btn[0].addEventListener('click', function () {
 
-for (let x = 0; x < friends.length; x++) {
-    for (let i = 99; i > 0; i--) {
-        if (i > 1) {
-            console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file ");
-        } else if (i = 1) {
-            console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file ");
-        } else {
-            console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes them out, clears it all out, no more lines of code in the file ");
+    let friends = ["Halle", "Jasmine", "Kimani", "Haily", "Jalon"];
+
+    for (let x = 0; x < friends.length; x++) {
+        let div = document.createElement('div');
+        div.className = 'friend';
+        document.body.appendChild(div);
+        console.log(friends[x]);
+
+        let h3 = document.createElement('h3');
+        let h3Text = document.createTextNode(friends[x]);
+        h3.append(h3Text);
+        div.appendChild(h3);
+
+
+        for (let i = 99; i > 0; i--) {
+
+            if (i > 1) {
+                let p = document.createElement('p');
+                let pText = document.createTextNode(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file ");
+                p.append(pText);
+                div.appendChild(p);
+
+            } else if (i = 1) {
+                let p = document.createElement('p');
+                let pText = document.createTextNode(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file ");
+                p.append(pText);
+                div.appendChild(p);
+
+            } else {
+                let p = document.createElement('p');
+                let pText = document.createTextNode(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes them out, clears it all out, no more lines of code in the file ");
+                p.append(pText);
+                div.appendChild(p);
+            }
         }
     }
-}
-
-// https://www.digitalocean.com/community/tutorials/how-to-construct-for-loops-in-javascript
+});
